410868ba5817db3ea5a38db6eb8886f4584a74b4
metroff <78957765+metroff@users.noreply.github.com>
2021-03-04 13:27:03 +0200

    Refactored grade processing

--- main.cpp
+++ main.cpp
@@ -155,33 +155,29 @@ void inputGrades(Student &student, int numOfGrades) {
     student.examGrade = getGrade();
 }
 
-// Skaičiuojamas vidurkis
-double findMean(vector<int> &array) {
-    if (array.empty())
-        return 0;
+// Skaičiuojamas pažymių vidurkis ir mediana
+void processGrades(vector<int> &array, double &mean, double &median) {
+    if (array.empty()) {
+        mean = 0;
+        median = 0;
+    }
+
+    int index = array.size() / 2;
     double sum = 0;
+    sort(array.begin(), array.end());
+    // Vidurkis
     for (auto number: array) {
         sum += number;
     }
-    return sum/array.size();
-}
-
-// Skaičiuojama mediana
-double findMedian(vector<int> &array) {
-    if (array.empty())
-        return 0;
-    sort(array.begin(), array.end());
-    int index = array.size() / 2;
-    return (array.size()%2==0) ? (double)(array[index-1]+array[index])/2.0 : (double)array[index];
+    mean = sum/array.size();
+    // Mediana
+    median = (array.size()%2==0) ? (double)(array[index-1]+array[index])/2.0 : (double)array[index];
 }
 
 // Skaičiuojamas galutinis pažymys
 void calculateFinalGrade(Student &student) {
-    // Nuadojant vidurkį
-    double mean = findMean(student.grades);
-    // Naudojant medianą
-    double median = findMedian(student.grades);
-
+    double mean, median;
+    processGrades(student.grades, mean, median);
     // Išvedamas galutinis pažymys
     double finalExamGrade = 0.6 * student.examGrade;
     student.finalMeanGrade = 0.4 * mean + finalExamGrade;
