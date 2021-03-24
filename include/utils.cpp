063df62ccceb21029c531a5611313c8ab84de636
metroff <78957765+metroff@users.noreply.github.com>
2021-03-24 13:29:54 +0200

    Sort for List

--- include/utils.cpp
+++ include/utils.cpp
@@ -1,5 +1,25 @@
 #include "utils.hpp"
 
+bool compareStudents(Student a, Student b) {
+    return (a.lastName == b.lastName) ? a.firstName < b.firstName : a.lastName < b.lastName;
+}
+
 bool isValidGrade(int grade){
     return (grade >= GRADE_MIN && grade <= GRADE_MAX);
+}
+
+void sortContainer(list<Student> &students){
+    students.sort();
+}
+
+void sortContainer(vector<Student> &students){
+    std::sort(students.begin(), students.end());
+}
+
+void sortContainerByName(list<Student> &students){
+    students.sort(compareStudents);
+}
+
+void sortContainerByName(vector<Student> &students){
+    std::sort(students.begin(), students.end(), compareStudents);
 }
--- include/utils.hpp
+++ include/utils.hpp
@@ -3,4 +3,14 @@
 #define GRADE_MIN 1
 #define GRADE_MAX 10
 
+#include "myLib.hpp"
+#include "struct.hpp"
+#include <algorithm>
+
 bool isValidGrade(int);
+
+void sortContainer(list<Student> &);
+void sortContainer(vector<Student> &);
+
+void sortContainerByName(list<Student> &);
+void sortContainerByName(vector<Student> &);
