b10b60e481eb9a4cb529bd39c66cfdf02ab62c90
metroff <78957765+metroff@users.noreply.github.com>
2021-04-29 00:30:16 +0300

    Added abstract human class

---
+++ include/human.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+class Human {
+    protected:
+        std::string firstName;
+        std::string lastName;
+
+    public:
+        Human(const std::string &_firstName = "", const std::string &_lastName = "") : firstName(_firstName), lastName(_lastName) {};
+
+        virtual std::string getFirstName() const = 0;
+        virtual std::string getLastName() const = 0;
+
+        void setFirstName(const std::string &firstName) {
+            this->firstName = firstName;
+        }
+
+        void setLastName(const std::string &lastName) {
+            this->lastName = lastName;
+        }
+};
